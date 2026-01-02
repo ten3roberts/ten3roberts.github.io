0f92cdc8d21f9d26064f05145dda11f596d79117
Freja Roberts <freja.mroberts@gmail.com>
2026-01-02 23:53:40 +0100

    chore: redirect

--- dist/assets/ProjectsView-CDuUeson.js
+++
@@ -1,1 +0,0 @@
-import{d as e,c as a,o,a as t,P as c}from"./index-D6RMhmEh.js";const m=e({__name:"ProjectsView",setup(n){return(r,s)=>(o(),a("main",null,[t(c)]))}});export{m as default};
--- src/views/ProjectsView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import Projects from '../components/Projects.vue'
-</script>
-
-<template>
-  <main>
-    <Projects />
-  </main>
-</template>
